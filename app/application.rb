a79748b17abb42f5c1e7c78607cba9c363dd890d
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 00:45:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     time = Time.new
 
-    if time.hour >= 12
+    if time.hour < 12 && time.min < 1
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
